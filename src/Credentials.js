cbaa814cbdfd1c378d84c45bc2ab1d1aac96ca41
Clay Spencer <clayspencer52@gmail.com>
2023-02-17 14:54:45 -0600

    done

---
+++ src/Credentials.js
@@ -0,0 +1,10 @@
+const Credentials = () => {
+
+    return {
+        ClientId: '470570277f8b439bbf85ad92f7d4e097',
+        ClientSecret: '32536e6af7d645b9829c4a7a8562b433'
+    }
+}
+
+export { Credentials };
+
